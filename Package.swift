5b9afd350fae6e862ed0d07fc70d40270c91c8f6
chenlidong <chenlidong@gmail.com>
2025-11-03 11:48:11 +0800

    hhhh

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "R2ClientKit",
+            type: .dynamic,
             targets: ["R2ClientKit"]
         ),
     ],
